25be5f8519b7aa629e21fac2838b6d6c140e5dc5
Zainab Rasool <jainab.rasool@gmail.com>
2022-07-09 17:04:43 +0530

    Update arraySum.java

--- my project/src/arraySum.java
+++ my project/src/arraySum.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class arraySum {
     public static void main(String[] a){
-        int arr[]=new int[]{1,2,3,4,5};
+        int arr[]=new int[]{1,4,2,9,0,11};
         System.out.println("Sum of digit Before index:"+Arrays.toString(calSumBeforeIndex(arr)));
         System.out.println("Sum of digit After index:"+Arrays.toString(calSumAfterIndex(arr)));
     }
@@ -19,12 +19,18 @@ public class arraySum {
         int n =a.length;
         int sum[] = new int[n];
 
-        sum[0]=n*(n+1)/2;
+        sum[0]=sum(a);
 
         for(int i=1;i<n;i++){
             sum[i]=sum[i-1]-a[i-1];
         }
         return  sum;
     }
-
+public static int sum(int a[]){
+        int sum=0;
+        for (int val:a){
+            sum+=val;
+        }
+     return  sum;
+}
 }
