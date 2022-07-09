1525d228fb4473b0e313982a4e40d3fd97e46721
Zainab Rasool <jainab.rasool@gmail.com>
2022-07-09 17:47:37 +0530

    Add files via upload

---
+++ my project/src/evenNumDigit.java
@@ -0,0 +1,25 @@
+public class evenNumDigit {
+    public static void  main(String []i){
+
+        int num[]=new int[]{-1,34,1234,-1100,0,-20};
+        System.out.println("Num of even digit in an array:: "+countDigit(num));
+    }
+    public static int countDigit(int a[]){
+
+        int d=0,digit=0;
+
+
+for(int val:a){
+
+    int len = String.valueOf(val).length();
+if(val<0){
+    val=-(val);
+    len= String.valueOf(val).length();
+}
+
+    if(len%2==0){
+    digit++;}
+}
+              return digit;
+    }
+}
